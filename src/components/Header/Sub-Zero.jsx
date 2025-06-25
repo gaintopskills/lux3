cd2ecf65e2070d60c0343e75ffb6feca7e6e066f
gaintopskills <raynafit@yahoo.com>
2025-06-25 10:43:02 -0700

    1

--- src/components/Header/Sub-Zero.jsx
+++ src/components/Header/Sub-Zero.jsx
@@ -30,7 +30,7 @@ const ParallaxHeader = () => {
         ✅ 1 year warranty on sealded system repair<br />
           ✅ Same-day or next day service<br />
         </p>
-        <p>When your Sub-Zero fridge isn’t cooling, your freezer’s too warm, or your wine cooler stops working—it’s not just inconvenient. It puts your food, wine, and peace of mind at risk. You can count on us to fix it.</p>
+        <p>If your Sub-Zero fridge not cooling, freezer’s too warm, or wine cooler stops working, you can count on us to fix it.</p>
       </div>
     </div>
   );
