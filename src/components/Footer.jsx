d2031b710dae3dae876465ea3a33cc2955c5b85b
gaintopskills <raynafit@yahoo.com>
2025-06-28 07:51:36 -0700

    1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -43,7 +43,6 @@ export const Footer = () => {
                 alt="Yelp"
                 className="yelp-icon"
               />
-             
             </a>
           </div>
         </div>
