b7ff2792e8fb60ef1e6ad56843f6f6b439fb64fa
gaintopskills <raynafit@yahoo.com>
2025-06-28 11:23:16 -0700

    1

--- src/components/Header/Sub-Zero-Brentwood.jsx
+++ src/components/Header/Sub-Zero-Brentwood.jsx
@@ -22,7 +22,7 @@ const ParallaxHeader = () => {
 
       <div className="maintext">
         <h1 class="gold-embossed" data-text="SUB-ZERO">Sub-Zero Repair Brentwood</h1>
-        <img className="header-image" width="400" height="210" src="/sub-zero/sub-zero-repair-services-brentwood.webp" alt="Sub-Zero Repair Services"/>
+        <img className="header-image" width="400" height="189" src="/sub-zero/sub-zero-repair-services-brentwood.webp" alt="Sub-Zero Repair Services"/>
         <p>
         Local Brentwood factory-trained Sub-Zero repair technicians specializing in luxury appliance repair including Sub-Zero repair.</p>
         <p>✅ Lisenced & Insured<br />
