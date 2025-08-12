b74d9a556f071fd64cba3d253260792169a53cda
gaintopskills <raynafit@yahoo.com>
2025-08-12 14:59:27 -0700

    1

--- src/components/MegaMenu.jsx
+++ src/components/MegaMenu.jsx
@@ -153,7 +153,7 @@ export const MegaMenu = () => {
       { label: "True Residential", href: "/true-residential-appliance-repair/" },
       { label: "Viking", href: "/viking-appliance-repair/" },
       { label: "Whirlpool Black Stainless", href: "/whirlpool-appliance-repair/" },
-      { label: "Wolf", href: "/wolf-appliance-repair/" },
+      { label: "Wolf", href: "/wolf/" },
       
       { label: "ZLINE", href: "/zline-appliance-repair/" },  
     ],
