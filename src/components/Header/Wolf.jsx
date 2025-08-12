856108d08ffda7756cdee18e8c06702fe56518fd
gaintopskills <raynafit@yahoo.com>
2025-08-12 14:05:23 -0700

    1

--- src/components/Header/Wolf.jsx
+++ src/components/Header/Wolf.jsx
@@ -20,17 +20,17 @@ const ParallaxHeader = () => {
         Your browser does not support the video tag.
       </video>
       <div className="maintext">
-        <h1 class="gold-embossed" data-text="SUB-ZERO">Sub-Zero Repair</h1>
-        <img className="header-image" width="1200" height="630" src="/sub-zero/sub-zero-repair-services.webp" alt="Sub-Zero Repair Services"  fetchpriority="high"
+        <h1 class="gold-embossed" data-text="Wolf">Wolf Appliance Repair</h1>
+        <img className="header-image" width="1200" height="630" src="/sub-zero/sub-zero-repair-services.webp" alt="Wolf Applaince Repair Services"  fetchpriority="high"
   decoding="async"/>
         <p>
-        Local, factory-trained Sub-Zero repair technicians specializing in luxury appliance repair including Sub-Zero appliances.</p>
+        Local, factory-trained Wolf repair technicians specializing in luxury appliance repair including Wolf appliances.</p>
         <p>✅ Lisenced & Insured<br />
         ✅ 3-month warranty on parts and labor<br />
         ✅ 1 year warranty on sealded system repair<br />
           ✅ Same-day or next day service<br />
         </p>
-        <p>If your Sub-Zero fridge not cooling, freezer’s too warm, or wine cooler stops working, you can count on us to fix it.</p>
+        <p>If your Wolf range won’t ignite, oven isn’t heating evenly, or cooktop controls stop responding, you can count on us to fix it.</p>
       </div>
     </div>
   );
