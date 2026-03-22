48726985d6231d9d7657b2b19c299c507b3ccbf5
gaintopskills <raynafit@yahoo.com>
2026-03-22 10:22:10 -0700

    1

---
+++ src/components/Header/Big-Chill.jsx
@@ -0,0 +1,38 @@
+// ParallaxHeader.jsx
+import React from 'react';
+
+const ParallaxHeader = () => {
+  return (
+    <div className="video-container">
+      <video
+        id="bgVideo"
+        className="bg-video"
+        autoPlay
+        muted
+        loop
+        playsInline
+      >
+        <source
+          src="/bg.mp4"
+          type="video/mp4"
+          media="(min-width: 769px)"
+        />
+        Your browser does not support the video tag.
+      </video>
+      <div className="maintext">
+        <h1 class="gold-embossed" data-text="SUB-ZERO">Big Chill Repair in Los Angeles & Nearby Areas</h1>
+        <img className="header-image" width="1200" height="630" src="/big-chill/big-chill-appliance-repair-services.webp" alt="Big Chill Appliance Repair Services"  fetchpriority="high"
+  decoding="async"/>
+        <p>
+        Factory-trained technicians for Big Chill refrigerators, ranges, dishwashers, ovens, stoves and range hoods across Los Angeles and nearby service areas.</p>
+        <p>✅ Licensed & Insured<br />
+        ✅ 3-month warranty on parts and labor<br />
+        ✅ 1-year warranty on sealed system repair<br />
+          ✅ Same-day or next-day service ae available<br />
+        </p>
+        <p>If your Big Chill refrigerator is not cooling, your freezer is running too warm, or your appliance is no longer performing the way it should, you can count on us for expert diagnosis and repair.</p>
+      </div>
+    </div>
+  );
+};
+export default ParallaxHeader;
