b25535d35aa6258a417c489a627a927ca6ff2d68
gaintopskills <raynafit@yahoo.com>
2026-03-25 15:54:52 -0700

    1

--- src/components/Header/Bertazzoni.jsx
+++ src/components/Header/Bertazzoni.jsx
@@ -20,18 +20,18 @@ const ParallaxHeader = () => {
         Your browser does not support the video tag.
       </video>
       <div className="maintext">
-        <h1 class="gold-embossed" data-text="Aga Appliance Repair">Aga Appliance Repair in Los Angeles & Nearby Areas</h1>
-        <img className="header-image" width="1200" height="630" src="/aga/aga-appliance-repair-services-trained-technicians.webp" alt="Aga Appliance Repair Services Los Angeles and Nearby Areas"  fetchpriority="high"
+        <h1 class="gold-embossed" data-text="Bertazzoni Appliance Repair in Los ANgeles and Nearby Areas">Bertazzoni Appliance Repair in Los ANgeles and Nearby Areas</h1>
+        <img className="header-image" width="1200" height="630" src="/bertazzoni/bertazzoni-appliance-repair-los-angeles-and-nearby-areas.webp" alt="Bertazzoni Appliance Repair Services Los Angeles and Nearby Areas"  fetchpriority="high"
   decoding="async"/>
     <p>
-  Factory-trained technicians providing expert AGA appliance repair for refrigerators, ranges, dishwashers, ovens, stoves, and range hoods throughout Los Angeles and surrounding service areas.
+  Factory-trained technicians providing expert Bertazzoni appliance repair for ranges, ovens, cooktops, dishwashers, wine coolers, and range hoods throughout Los Angeles and surrounding service areas.
 </p>
         <p>✅ Licensed & Insured<br />
         ✅ 3-month warranty on parts and labor<br />
         ✅ 1-year warranty on sealed system repair<br />
           ✅ Same-day or next-day service ae available<br />
         </p>
-        <p>  If your AGA range is not heating properly, your burners are not igniting as they should, or your oven is no longer performing the way it should, you can count on us for expert diagnosis and repair.</p>
+        <p>If your Bertazzoni range is not heating properly, your cooktop burners are not igniting as they should, or your Bertazzoni oven is no longer performing the way it should, you can count on us for expert diagnosis and repair.</p>
     </div>
     </div>
   );
