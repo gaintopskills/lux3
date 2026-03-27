414d04eb0a2e5015a067a5c968baa5c79ee4b4c8
gaintopskills <raynafit@yahoo.com>
2026-03-27 10:57:14 -0700

    1

--- src/components/Header/Cove.jsx
+++ src/components/Header/Cove.jsx
@@ -20,15 +20,14 @@ const ParallaxHeader = () => {
         Your browser does not support the video tag.
       </video>
       <div className="maintext">
-        <h1 class="gold-embossed" data-text="Cove Appliance Repair">Cove Appliance Repair in Los Angeles & Nearby Areas</h1>
-        <img className="header-image" width="1200" height="630" src="/cove/cove-dishwasher-repair-los-angeles-and-nearby-areas.webp" alt="Cove Appliance Repair Services Los Angeles and Nearby Areas"  fetchpriority="high"
+        <h1 class="gold-embossed" data-text="Cove Dishwasher Repair">Cove Dishwasher Repair in Los Angeles & Nearby Areas</h1>
+        <img className="header-image" width="1200" height="630" src="/cove/cove-dishwasher-repair-los-angeles-and-nearby-areas.webp" alt="Cove Dishwasher Repair Services Los Angeles and Nearby Areas"  fetchpriority="high"
   decoding="async"/>
     <p>
     Factory-trained technicians providing expert Cove dishwasher repair throughout Los Angeles and surrounding service areas.
 </p>
         <p>✅ Licensed & Insured<br />
         ✅ 3-month warranty on parts and labor<br />
-        ✅ 1-year warranty on sealed system repair<br />
           ✅ Same-day or next-day service ae available<br />
         </p>
         <p>If your Cove dishwasher is not cleaning properly, not draining as it should, or no longer performing the way it should, you can count on us for expert diagnosis and repair.</p>
