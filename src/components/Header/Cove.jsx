4d9bd18ca847724a6ff73ff192e55e4bbf535cb8
gaintopskills <raynafit@yahoo.com>
2026-03-27 10:24:44 -0700

    1

---
+++ src/components/Header/Cove.jsx
@@ -0,0 +1,39 @@
+// ParallaxHeader.jsx
+import React from 'react';
+
+const ParallaxHeader = () => {
+  return (
+    <div className="video-container">
+      <video
+        id="bgVideo"
+        className="bg-video"
+        autoPlay
+        muted
+        loop
+        playsInline
+      >
+        <source
+          src="/bg.mp4"
+          type="video/mp4"
+          media="(min-width: 769px)"
+        />
+        Your browser does not support the video tag.
+      </video>
+      <div className="maintext">
+        <h1 class="gold-embossed" data-text="Cove Appliance Repair">Cove Appliance Repair in Los Angeles & Nearby Areas</h1>
+        <img className="header-image" width="1200" height="630" src="/cove/cove-dishwasher-repair-los-angeles-and-nearby-areas.webp" alt="Cove Appliance Repair Services Los Angeles and Nearby Areas"  fetchpriority="high"
+  decoding="async"/>
+    <p>
+    Factory-trained technicians providing expert Cove dishwasher repair throughout Los Angeles and surrounding service areas.
+</p>
+        <p>✅ Licensed & Insured<br />
+        ✅ 3-month warranty on parts and labor<br />
+        ✅ 1-year warranty on sealed system repair<br />
+          ✅ Same-day or next-day service ae available<br />
+        </p>
+        <p>If your Cove dishwasher is not cleaning properly, not draining as it should, or no longer performing the way it should, you can count on us for expert diagnosis and repair.</p>
+    </div>
+    </div>
+  );
+};
+export default ParallaxHeader;
