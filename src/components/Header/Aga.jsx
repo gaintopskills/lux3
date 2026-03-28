e0f33c41b2263ef6f8fffd19871cc7255e68ed00
gaintopskills <raynafit@yahoo.com>
2026-03-28 10:04:52 -0700

    1

--- src/components/Header/Aga.jsx
+++ src/components/Header/Aga.jsx
@@ -23,10 +23,7 @@ const ParallaxHeader = () => {
         <h1 class="gold-embossed" data-text="AGA Appliance Repair">AGA Appliance Repair in Los Angeles & Nearby Areas</h1>
         <img className="header-image" width="1200" height="630" src="/aga/aga-appliance-repair-services-trained-technicians.webp" alt="AGA Appliance Repair Services Los Angeles and Nearby Areas"  fetchpriority="high"
   decoding="async"/>
-    <p>
-  Factory-trained technicians providing expert AGA appliance repair for refrigerators, ranges, dishwashers, ovens, stoves, and range hoods throughout Los Angeles and surrounding service areas.
-</p>
-        <p>✅ Licensed & Insured<br />
+<p>Factory-trained technicians providing expert AGA appliance repair for ranges, range cookers, rangetops, ovens, and hoods throughout Los Angeles and surrounding service areas.</p>        <p>✅ Licensed & Insured<br />
         ✅ 3-month warranty on parts and labor<br />
         ✅ 1-year warranty on sealed system repair<br />
           ✅ Same-day or next-day service ae available<br />
