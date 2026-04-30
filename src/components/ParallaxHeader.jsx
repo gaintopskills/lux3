12c53f920918386be0095553da41884fab928289
gaintopskills <raynafit@yahoo.com>
2026-04-30 14:01:21 -0700

    1

--- src/components/ParallaxHeader.jsx
+++ src/components/ParallaxHeader.jsx
@@ -25,15 +25,13 @@ const ParallaxHeader = () => {
         <img className="header-image" width="1200" height="630" src="/high-end-and-luxury-appliance-repair-by-zaricci.webp" alt="High-End and Luxury Appliance Repair Los Angeles and Nearby Areas"  fetchpriority="high"
   decoding="async"/>
         <p>
-          At Luxury Appliance Repair, we specialize in repairing high-end appliances with
-          precision, ensuring your kitchen and home run seamlessly. With certified and highly expirienced 
-          technicians, premium-quality parts, and exceptional customer care, we deliver
-          reliable service tailored to your needs.
+          At Luxury Appliance Repair by Zaricci, we specialize in repairing high-end appliances with
+          precision. We offer certified and highly expirienced 
+          technicians, premium-quality parts, and exceptional customer care.
         </p>
         <p>
           We’re trusted by our customers to repair luxury and high-end
-          brands like Viking, Wolf, Thermador, Gaggenau, Miele, Bosch, Jenn-Air,
-          KitchenAid, Dacor, La Cornue, Smeg, Big Chill, Elmira Stove Works, BlueStar,
+          brands like La Cornue, Bertazzoni, Gaggenau, BlueStar, Viking, Wolf, Thermador, Miele, Bosch, Dacor, Smeg, Big Chill, Elmira Stove Works,
           Fisher & Paykel, AGA, and more.
         </p>
       </div>
