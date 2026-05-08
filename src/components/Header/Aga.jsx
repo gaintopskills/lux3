93af4a2f1fd4682215a52f164c2d66a7c9d2b7dd
gaintopskills <raynafit@yahoo.com>
2026-05-08 14:51:54 -0700

    1

--- src/components/Header/Aga.jsx
+++ src/components/Header/Aga.jsx
@@ -20,10 +20,10 @@ const ParallaxHeader = () => {
         Your browser does not support the video tag.
       </video>
       <div className="maintext">
-        <h1 class="gold-embossed" data-text="AGA Appliance Repair">AGA Appliance Repair in Los Angeles & Nearby Areas</h1>
+        <h1 class="gold-embossed" data-text="AGA Appliance Repair">AGA Appliance Repair by Zaricci</h1>
         <img className="header-image" width="1200" height="630" src="/aga/aga-appliance-repair-services-trained-technicians.webp" alt="AGA Appliance Repair Services Los Angeles and Nearby Areas"  fetchpriority="high"
   decoding="async"/>
-<p>Experienced technicians specializing in luxury AGA appliance repair. We are experts in fixing ranges, range cookers, rangetops, ovens, and hoods throughout Los Angeles and nearby areas.</p>        
+<p>Experienced technicians specializing in luxury AGA appliance repair. We are experts in fixing ranges, range cookers, rangetops, ovens, and hoods.</p>        
 <p>✅ Licensed & Insured<br />
         ✅ 3-month warranty on parts and labor<br />
           ✅ Same-day or next-day appointments are available<br />
