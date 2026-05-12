a6948d3bdd4bfc6c4fcd5e0a48569d617082d84c
gaintopskills <raynafit@yahoo.com>
2026-05-12 08:16:53 -0700

    1

--- src/components/Header/Aga.jsx
+++ src/components/Header/Aga.jsx
@@ -20,7 +20,9 @@ const ParallaxHeader = () => {
         Your browser does not support the video tag.
       </video>
       <div className="maintext">
-        <h1 class="gold-embossed" data-text="AGA Appliance Repair">AGA Appliance Repair by Zaricci</h1>
+        <h1 class="gold-embossed" data-text="AGA Appliance Repair">AGA Appliance Repair<br/> <span className="h1-location">
+    in Los Angeles, Orange County & Ventura County
+  </span></h1>
         <img className="header-image" width="1200" height="630" src="/aga/aga-appliance-repair-services-trained-technicians.webp" alt="AGA Appliance Repair Services Los Angeles and Nearby Areas"  fetchpriority="high"
   decoding="async"/>
 <p>Experienced technicians specializing in luxury AGA appliance repair. We are experts in fixing ranges, range cookers, rangetops, ovens, and hoods.</p>        
