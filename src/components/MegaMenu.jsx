08d7849c639c8b85b0f466f61c7b28012c8d516f
gaintopskills <raynafit@yahoo.com>
2026-05-12 21:26:56 -0700

    1

--- src/components/MegaMenu.jsx
+++ src/components/MegaMenu.jsx
@@ -181,7 +181,7 @@ export const MegaMenu = () => {
       { label: "Viking Outdoor", href: "/viking-appliance-repair/" },
       { label: "DCS", href: "/dcs-appliance-repair/" },
       { label: "Alfresco", href: "/alfresco-appliance-repair/" },
-      { label: "Twin Eagles", href: "/twin-eagles-appliance-repair/" },
+      { label: "Twin Eagles", href: "/twin-eagles-repair/" },
       { label: "Fire Magic", href: "/fire-magic-appliance-repair/" },
       { label: "Wolf Outdoors", href: "/wolf-appliance-repair/" },
       { label: "True Residential Outdoor", href: "/true-residential-appliance-repair/" },
