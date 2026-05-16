db7f7281b34b78074c8b6de805ac6b52560f1fa9
gaintopskills <raynafit@yahoo.com>
2026-05-16 15:50:57 -0700

    1

--- src/components/ReviewsHomePage.jsx
+++ src/components/ReviewsHomePage.jsx
@@ -416,7 +416,7 @@ export default function GoogleReviews() {
 
             <div className="summary-text">
               <strong>
-                30+ Google reviews
+                40+ Google reviews
               </strong>
 
               <span>
