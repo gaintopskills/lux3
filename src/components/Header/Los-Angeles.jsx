1c4ef275d0da816658baf946bba84a67cf4925ca
gaintopskills <raynafit@yahoo.com>
2026-05-17 13:25:19 -0700

    1

--- src/components/Header/Los-Angeles.jsx
+++ src/components/Header/Los-Angeles.jsx
@@ -26,11 +26,11 @@ const ParallaxHeader = () => {
   </span></h1>
         <img className="header-image" width="400" height="189" src="/cities/luxury-appliance-repair-los-angeles-519x272.webp" alt="High-end appliance repair Los Angeles" fetchpriority="high"
   decoding="async"/>
-        <p>
-        We provide Los Angeles local high-end appliance repair services specializing in luxury appliance repair including Sub-Zero, Wolf, Viking, Thermador, Cove, Miele, Bosch and more. We repair all major appliances such as refrigerators, freezers, ranges, ovens, washers, dryers, grills, and more.</p>
+       <p>
+          Professional repair for luxury and high-end appliances in Los Angeles, including Sub-Zero, Wolf, Viking, Thermador, Miele, Bosch, Cove, and other premium brands.
+        </p>
         <p>✅ Licensed & Insured<br />
         ✅ 3-month warranty on parts and labor<br />
-        ✅ Trained<br />
           ✅ Same-day or next day service<br />
         </p>
       </div>
