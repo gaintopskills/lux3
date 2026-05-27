02aa8dcb6715a8892cdd86e2b7e374e3ca8c4170
gaintopskills <raynafit@yahoo.com>
2026-05-27 08:51:51 -0700

    1

--- src/components/Header/BelAir.jsx
+++ src/components/Header/BelAir.jsx
@@ -22,12 +22,12 @@ const ParallaxHeader = () => {
 
       <div className="maintext">
         <h1 class="gold-embossed" data-text="SUB-ZERO">High-End & Luxury Applliance Repair<br/> <span className="h1-location">
-    in Los Angeles
+    in Bel Air
   </span></h1>
-        <img className="header-image" width="400" height="189" src="/cities/luxury-appliance-repair-los-angeles-519x272.webp" alt="High-end appliance repair Bel Air" fetchpriority="high"
+        <img className="header-image" width="400" height="189" src="/high-end-and-luxury-appliance-repair-by-zaricci.webp" alt="High-end appliance repair Bel Air" fetchpriority="high"
   decoding="async"/>
        <p>
-          Professional repair for luxury and high-end appliances in Bel Air, including Sub-Zero, Wolf, Viking, Thermador, Miele, Bosch, Cove, and other premium brands.
+          Professional repair for luxury and high-end appliances in Bel Air, including La Cornue, Lacanche, Bertazzoni, Sub-Zero, Wolf, Viking, Thermador, Miele, Bosch, Cove, and other premium brands.
         </p>
         <p>✅ Licensed & Insured<br />
         ✅ 3-month warranty on parts and labor<br />
