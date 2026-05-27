8014dadaaef791fd50c42bcfd232a009a2148816
gaintopskills <raynafit@yahoo.com>
2026-05-27 10:16:53 -0700

    1

--- src/components/Header/BelAir.jsx
+++ src/components/Header/BelAir.jsx
@@ -21,14 +21,12 @@ const ParallaxHeader = () => {
       </video>
 
       <div className="maintext">
-        <h1 class="gold-embossed" data-text="SUB-ZERO">High-End & Luxury Applliance Repair<br/> <span className="h1-location">
-    in Bel Air
-  </span></h1>
+        <h1 class="gold-embossed" data-text="SUB-ZERO">Bel Air High-End & Luxury Appliance Repair<br/></h1>
         <img className="header-image" width="400" height="189" src="/high-end-and-luxury-appliance-repair-by-zaricci.webp" alt="High-end appliance repair Bel Air" fetchpriority="high"
   decoding="async"/>
-       <p>
-          Professional repair for high-end and luxury appliances in Bel Air, including La Cornue, Lacanche, Gaggenau, Bertazzoni, Monogram, Dacor, Fisher & Paykel, True, Sub-Zero, Wolf, Viking, Thermador, Miele, Bosch, Cove, and other premium brands.
-        </p>
+        <p>
+  Professional high-end appliance repair in Bel Air for luxury built-in refrigerators, ranges, ovens, cooktops, dishwashers, wine coolers, laundry appliances, and outdoor kitchen systems. We service Sub-Zero, Wolf, Thermador, Miele, Viking, Gaggenau, La Cornue, Lacanche, Monogram, Dacor, Fisher & Paykel, Bertazzoni, True Residential, JennAir, Bosch, Kalamazoo, Lynx, Hestan, DCS, Alfresco, Twin Eagles, Fire Magic, and other premium appliance brands.
+</p>
         <p>✅ Licensed & Insured<br />
         ✅ 3-month warranty on parts and labor<br />
           ✅ Same-day or next day service<br />
